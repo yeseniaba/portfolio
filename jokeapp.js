7bf6bddcf671beef5c7abcb8b0ce091c28b756ef
yeseniaba <73720916+yeseniaba@users.noreply.github.com>
2021-07-12 08:39:13 -0700

    Removing excess

--- jokeapp.js
+++ jokeapp.js
@@ -1,12 +1,8 @@
 document.addEventListener("click", function (event) {
-  // Checking if the button was clicked
+
   if (!event.target.matches("#button")) return;
 
-  /*
-  Here we send a request to the Joke API
-  Then process the response into JSON
-  Then pass the data to our renderJoke function.
-  */
+
   fetch("https://official-joke-api.appspot.com/random_joke")
     .then((response) => response.json())
     .then((data) => renderJoke(data))
@@ -14,12 +10,12 @@ document.addEventListener("click", function (event) {
 });
 
 function renderJoke(data) {
-  // Get text elements
+
   const setup = document.getElementById("setup");
   const punchline = document.getElementById("punchline");
   const error = document.getElementById("error");
 
-  // Hide error and render jokes
+
   error.innerHTML = "";
   setup.innerHTML = data.setup;
   punchline.innerHTML = data.punchline;
